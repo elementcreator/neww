849c15891b8672b336ab246cb40a80f0ff2f21e8
elementcreator <60968627+elementcreator@users.noreply.github.com>
2020-03-09 17:39:16 +0500

    Add files via upload

---
+++ Zarabotnaya plata/Zarabotnaya plata/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace Zarabotnaya_plata
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection("Data Source = 303-6\\SQLSERVER; Initial Catalog = Zarplati ; Integrated Security = true;");
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand com = new SqlCommand($"Select [FIO_manager] From manager where Login_manager = '{textBox1.Text}' and Password ='{textBox2.Text}'",con);
+            SqlDataReader dr = com.ExecuteReader();
+            if (dr.HasRows)
+            {
+                dr.Read();           
+                MessageBox.Show("Вход выполнен");
+                Form2 fm = new Form2();
+                dr.Close();
+                con.Close();
+                fm.Show();
+                this.Hide();
+            }
+            else
+                MessageBox.Show("Неверно введен пароль или логин");
+
+           if (con.State == ConnectionState.Open)
+                con.Close();
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Zarabotnaya plata/Zarabotnaya plata/Form2.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Zarabotnaya_plata
+{
+    public partial class Form2 : Form
+
+    {
+        SqlConnection con = new SqlConnection("Data Source = 303-6\\SQLSERVER; Initial Catalog = Zarplati ; Integrated Security = true;");
+
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+
+        }
+        public int id;
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand com = new SqlCommand($"Select * From coeefs, grade Where coeefs.id='{id}' AND coeefs.id=grade.id",con);
+            SqlDataReader dr = com.ExecuteReader(); 
+            if (dr.HasRows)
+            {
+                dr.Read();
+                textBox1.Text = dr["Junior"].ToString();
+                textBox2.Text = dr["Middle"].ToString();
+                textBox3.Text = dr["Senior"].ToString();
+                textBox4.Text = dr["Koef_Analiz"].ToString();
+                textBox5.Text = dr["Koef_device"].ToString();
+                textBox6.Text= dr["Koef_servicez"].ToString();
+                textBox7.Text= dr["Koef_time"].ToString();
+                textBox8.Text= dr["coef_complexity"].ToString();
+                textBox9.Text = dr["Koef_money"].ToString();
+                dr.Close();
+            }
+
+            con.Close();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
